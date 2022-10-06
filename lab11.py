dfeb98f5f0d6a70dcc7194f6201867a8683108a3
c0-0per <115171974+c0-0per@users.noreply.github.com>
2022-10-06 18:03:52 +0200

    Update lab11.py

--- lab11.py
+++ lab11.py
@@ -2,9 +2,9 @@ import argparse
  
 parser = argparse.ArgumentParser() 
  
-parser.add_argument("a", type=float, help='first number') 
-parser.add_argument("action", help='operations with numbers') 
-parser.add_argument("b", type=float, help='second number') 
+parser.add_argument('a', type = float , help = 'first number') 
+parser.add_argument('action', help = 'operations with numbers') 
+parser.add_argument('b', type = float, help = 'second number') 
  
 args = parser.parse_args() 
      
@@ -18,8 +18,8 @@ elif args.action == "/":
     try: 
         print(args.a / args.b) 
     except ZeroDivisionError: 
-        print('Invalid data entered\nDivision by zero') 
+        raise ValueError('This operation is not supported for given input parameters')
 else: 
-    print('Invalid data entered')
+    raise ValueError('This operation is not supported for given input parameters')
     
     
